e2081aa717085561de0e07455cfd5685cc97832b
ValeriaPerez22 <vp1822iq@gmail.com>
2026-05-11 20:08:20 -0600

    index

---
+++ index.js
@@ -0,0 +1,24 @@
+let productos= prompt("Ingresa la letra en MAYÚSCULA del paquete que deseas o el número si es un solo artículo)\n" +
+ "Libreta ($99.99),     Calculadora($340),        Diccionario($70.99)\n" +                  
+"Regla($5.99),          Boligrafo($15.10),        Papel adhesivo ($16.99)\n" + 
+"Lapicera ($150.99),    Archivero ($490.99),      Pisapapeles ($45.10)\n" +
+"Clips ( $13.99),       Tijeras ($35),        Libreta ($99.99)\n" +
+"Calculadora($340),     Diccionario($70.99),     Regla($5.99)\n" +
+"Boligrafo($15.10),     Papel adhesivo ($16.99), Lapicera ($150.99)\n" +
+"Archivero ($490.99),    Pisapapeles ($45.10),     Clips ( $13.99)\n" +
+"Tijeras ($35)\n" +
+"A)     PaqueteUno: Contiene(Libreta ($99.99),Calculadora($340),Diccionario($70.99),Regla($5.99), Boligrafo($15.10)) \n" +
+"B)     PaqueteDos: Contiene (Libreta ($99.99), Calculadora($340), Diccionario($70.99), Regla($5.99),Boligrafo($15.10), Papel adhesivo ($16.99)\n" + 
+"Lapicera ($150.99),Archivero ($490.99), Pisapapeles ($45.10), Clips ( $13.99)\n" +
+"D)     PaqueteTres: Todos los artículos" 
+);
+
+
+
+if ( productos = 1 ) {
+    console.log("Has elegido el paquete uno")
+} else  (productos) {
+    console.log("No puedes e")
+}
+
+
